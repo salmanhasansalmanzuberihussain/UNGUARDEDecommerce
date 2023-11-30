5d1810d8bfed066aacd387616e4ee2687183c799
Salman Hasan <s.hasan@unguarded.com>
2023-11-30 16:30:53 -0500

    First Commit

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const port = 8000;
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const jwt = require('jsonwebtoken');
+
+mongoose
+  .connect(
+    'mongodb+srv://sh8salmanhasan:Hasan732****@cluster0.2dywbrs.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+
+  .catch((err) => {
+    console.log('Error connecting to MongoDB', err);
+  });
+
+app.listen(port, () => {
+  console.log('Server is running on port 8000');
+});
