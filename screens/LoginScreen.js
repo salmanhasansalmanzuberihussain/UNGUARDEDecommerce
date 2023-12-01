d7e2810784732149433de8c172009e0af6b80c55
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-12-01 07:39:39 -0500

    Add files via upload

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+} from 'react-native';
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: 'white', alignItems: 'center' }}
+    >
+      <View>
+        <Image
+          style={{ width: 150, height: 100 }}
+          source={{
+            uri: 'https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png',
+          }}
+        />
+      </View>
+
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: 'center' }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: 'bold',
+              marginTop: 12,
+              color: '#041E42',
+            }}
+          >
+            Login in to your account
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 70 }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 5,
+              backgroundColor: '#D0D0D0',
+              paddingVertical: 5,
+              borderRadius: 5,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="black"
+            />
+            <TextInput
+              style={{ color: 'gray', marginVertical: 10, width: 300 }}
+              placeholder="enter your Email"
+            />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
