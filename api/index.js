3bf5377ddb228074c3aa570bfe15dee430be46e9
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-12-03 08:39:47 -0500

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -29,3 +29,45 @@ mongoose
 app.listen(port, () => {
   console.log('Server is running on port 8000');
 });
+
+const User = require('./models/user');
+const Order = require('./models/order');
+
+//function to send Verification Email to the user
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'sh8salmanhasan@gmail.com',
+      pass: '',
+    },
+  });
+};
+
+//end point to register in the application
+app.post('/register', async (req, res) => {
+  try {
+    const { name, emai, password } = req.body;
+
+    //check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already registered' });
+    }
+
+    //create a new User
+    const newUser = new User({ name, email, password });
+
+    //generate and store the verification token
+    newUser.verificationToken = crytpo.randomBytes(20).toString('hex');
+
+    //save the user to the database
+    await newUser.save();
+
+    //send verification email to the user
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+  } catch (error) {
+    console.log('error registering user', error);
+    res.status(500).json({ message: 'Registration failed' });
+  }
+});
